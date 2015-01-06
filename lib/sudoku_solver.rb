4dee378653ab0af86de1dadad28506069281a29a
ajn123 <ajn123@vt.edu>
2015-01-05 21:30:36 -0500

    added grid and test skeloton

--- lib/sudoku_solver.rb
+++ lib/sudoku_solver.rb
@@ -3,6 +3,7 @@ require "sudoku_solver/cell"
 require "sudoku_solver/box"
 require 'sudoku_solver/row'
 require 'sudoku_solver/column'
+require 'sudoku_solver/grid'
 module SudokuSolver
   # Your code goes here...
 end
---
+++ lib/sudoku_solver/grid.rb
@@ -0,0 +1,13 @@
+
+
+
+class Grid
+  attr_accessor :rows, :columns, :boxes
+  def initialize
+
+  end
+
+  def load(txt_file)
+          
+  end
+end
