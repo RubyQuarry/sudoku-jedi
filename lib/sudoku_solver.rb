d4e04033a2ad1f9f76f9a43539fa479fd9341500
ajn123 <ajn123@vt.edu>
2015-01-05 21:03:17 -0500

    updated lib requires

--- lib/sudoku_solver.rb
+++ lib/sudoku_solver.rb
@@ -1,7 +1,8 @@
 require "sudoku_solver/version"
 require "sudoku_solver/cell"
 require "sudoku_solver/box"
-
+require 'sudoku_solver/row'
+require 'sudoku_solver/column'
 module SudokuSolver
   # Your code goes here...
 end
