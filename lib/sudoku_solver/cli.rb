a8dea94f7cc56118c90496457e172a13ecf26f01
ajn123 <ajn123@vt.edu>
2015-01-18 05:53:04 -0500

    added cli environment

---
+++ lib/sudoku_solver/cli.rb
@@ -0,0 +1,13 @@
+require 'thor'
+require 'grid'
+class CLI < Thor
+
+  desc "solve", "Solves a sudoku puzzle"
+  def solve(file_name)
+  	file = File.open(file_name,"rb")
+  	contents = file.read
+    grid = Grid.new([contents])
+    grid.solve
+    grid.print_values_formatted
+  end
+end
