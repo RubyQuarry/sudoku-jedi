006694c8818a5314db56a960c94833aebdd2a4b1
ajn123 <ajn123@vt.edu>
2015-01-18 05:52:29 -0500

    added executable to sudoku

--- lib/sudoku_solver.rb
+++ lib/sudoku_solver.rb
@@ -4,6 +4,7 @@ require "sudoku_solver/box"
 require 'sudoku_solver/row'
 require 'sudoku_solver/column'
 require 'sudoku_solver/grid'
+require 'sudoku_solver/cli'
 module SudokuSolver
   # Your code goes here...
 end
