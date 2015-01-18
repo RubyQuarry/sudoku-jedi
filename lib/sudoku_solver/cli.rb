65ee8a4234cc085b31ec9d726f0da3301340910d
ajn123 <ajn123@vt.edu>
2015-01-18 06:09:11 -0500

    fixed cli

--- lib/sudoku_solver/cli.rb
+++ lib/sudoku_solver/cli.rb
@@ -5,8 +5,9 @@ class CLI < Thor
   desc "solve", "Solves a sudoku puzzle"
   def solve(file_name)
   	file = File.open(file_name,"rb")
-  	contents = file.read
-    grid = Grid.new([contents])
+  	contents = file.read.gsub("\n","")
+  	contents.gsub!(" ","")
+    grid = Grid.new(contents.scan(/\d{9}/))
     grid.solve
     grid.print_values_formatted
   end
